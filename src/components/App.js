053c71995dbd83ceeb4758419209b7eab62eb43b
vincenttommi <vincenttommi@gmail.com>
2023-04-26 15:26:59 +0300

    correcting data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+
+const [dogImage, setDogImage] = useState(null);
+
+
+useEffect(()=>{
+ fetch("https://dog.ceo/api/breeds/image/random")
+
+ 
+ .then(response => response.json())
+ .then(data => {
+    setDogImage(data.message);
+  })
+  .catch(error => {
+    console.error('Error fetching random dog image:', error);
+  });
+}, []);
+
+return (
+    <div className="App">
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
